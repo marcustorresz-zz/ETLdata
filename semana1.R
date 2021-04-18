c353393f63fadd6373088ad85a6f54e90ba4a7c3
Marcus Torres <marvinist@hotmail.com>
2021-04-18 15:44:10 -0300

    Tentar commit de novo

--- semana1.R
+++ semana1.R
@@ -15,3 +15,20 @@ str(iris)
 #############OBJETOS E FUNÇÕES DO R ###############
 
 c <- c(2, 3, 4, 5, 6) # Criando o vetor c
+d <- c(-1,3,2,4,1) #criando o vetor d
+
+#####Análises multivariadas####
+
+#Correlação entre as variáveis 'c' e 'd'
+cor(c,d)
+
+
+#Regressão entre as mesmas variáveis
+
+lm(c~d)
+lm(d~c)
+
+
+# Para ver a complexidade do banco, usar str(), como nesse caso abaixo
+
+str(d)
